6bc62242b6a4ce453c4ac25f3975a3fcc02aa2b7
Adyasha Priyadarshini Sahoo <108588681+adyasha-official@users.noreply.github.com>
2025-12-17 14:21:15 +0000

    conversion of datatypes

---
+++ 01_Basics/conversion_of_operations.js
@@ -0,0 +1,26 @@
+// converting string to number 
+let score = "33";
+let age = 22;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+console.log(typeof age);
+console.log(typeof (age));
+
+// convert the string 'score' to number 
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+let rule = "23abc";
+let convert = Number(rule);
+console.log(typeof convert);
+console.log(convert);
+// it converts into number but when we actually see it we don't get the value instead NaN is printed
+
+// for boolean
+// 1 => true; 0 => false
+// "" => empty string => false
+// "adyasha" => true
+ 
+
