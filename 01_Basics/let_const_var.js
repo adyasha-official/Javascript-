3f9024d43fc433d677d81295931f8b61dc5f02df
Adyasha Priyadarshini Sahoo <108588681+adyasha-official@users.noreply.github.com>
2025-12-17 11:44:48 +0000

    Learning the terms const, var and let

---
+++ 01_Basics/let_const_var.js
@@ -0,0 +1,19 @@
+// whenever these keywords are found assign it to the memory
+
+// declaring kwyword const
+// value inside const can't be changed 
+const accountId = 2253015;
+
+// declaring keyword let 
+// value inside can be changed
+let accountEmail = "adyasha@gmail.com";
+
+// declaring keyword var
+// value inside can be changed
+var accountPassword = "Adyasha";
+accountCity = "Angul";
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+// used to print the values in a table format
+
+// in modern js let is used instead of var because var doesn't work in scope 
