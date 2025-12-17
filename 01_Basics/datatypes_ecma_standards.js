968e2400ffd1ced225a4e73ea6713e43c8827b33
Adyasha Priyadarshini Sahoo <108588681+adyasha-official@users.noreply.github.com>
2025-12-17 12:12:50 +0000

    datatypes

---
+++ 01_Basics/datatypes_ecma_standards.js
@@ -0,0 +1,20 @@
+// treat all js code as newer version
+"use strict";
+
+// alert (3+3) 
+// it will not give output as we are in node.js not browser 
+// The alert() method in JavaScript displays a modal alert box with a specified message and an "OK" button
+
+// number => 2**53 (range in memory addess)
+// bigint for larger value of numbers
+// string
+// boolean
+// null => standalone value
+// undefined => value is not assigned
+// symbol => to define unique components
+// object
+
+let age = 22;
+let name = "Adyasha";
+console.log(typeof age);
+console.log(typeof name);
